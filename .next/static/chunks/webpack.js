4cd664310e752282874bb4cae18c0f73bd08c5e1
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2023-03-22 14:35:05 -0700

    added gitignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e606b33bdecbf3fe5105"; }
+/******/ 		__webpack_require__.h = function() { return "fb697e11920053270a6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
