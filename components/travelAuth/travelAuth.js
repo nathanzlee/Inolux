e89a264cbba5dd1e4ec9b2e92cfdddddcab46d30
Nathan Lee <worldnumber1pingpongplayer@gmail.com>
2023-03-22 19:30:03 -0700

    package.json change

--- components/travelAuth/travelAuth.js
+++ components/travelAuth/travelAuth.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import Router from 'next/router'
 import PersonalInfo from './sections/personalInfo'
 import InternationalTravel from './sections/internationalTravel'
